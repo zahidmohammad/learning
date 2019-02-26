6328c9648eb111570d4e0d7ef4f8940a957bc9c5
zahidmohammad <zahidmohammad392@gmail.com>
2019-02-26 17:05:06 +0530

    files added

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+
+<h1>Contact Pages</h1>
+
+@if (count($people))
+
+<ul>
+
+@foreach($people as $person)
+
+<li>{{$person}}</li>
+
+
+@endforeach
+
+</ul>
+
+@endif
+
+
+
+
+
+@stop
---
+++ routes/web.php
@@ -0,0 +1,193 @@
+<?php
+
+use App\Post;
+use Illuminate\Support\Facades\DB;
+// use App\Post;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+// |
+// */
+
+//  Route::get('/', function () {
+//      return view('welcome');
+//  });
+
+//  Route::get('/about', function () {
+//      return 'Hi about pages';
+//  });
+
+//  Route::get('/contact', function () {
+//      return 'Hi Iam contact pages';
+//  });
+
+//  Route::get('/posts/{id}/{name}', function ($id, $name) {
+//      return 'This is the posts number '.$id.' '.$name;
+//  });
+
+//  Route::get('admin/posts/example', array('as'=>'admin.home' , function(){
+
+//    $url = route('admin.home');
+//    return "This url is ". $url;
+
+//  }));
+
+// // Route::get('/post/{id}', 'PostsController@index');
+// Route::resource('/posts', 'PostsController');
+
+// Route::get('/contact', 'PostsController@contact');
+ 
+// Route::get('/post/{id}/{name}/{password}', 'PostsController@show_post');
+
+
+//////////////// --------------------DATABASE-----RAW SQL Queries----------------------------------------------------////////////////////////////
+
+
+//  Route::get('/insert', function(){
+
+//   DB::insert('insert into posts(title,content) values(?,?)', ['Laravel is owesome with Edwin', 'Laravel is the best thing that has happened to PHP,PERIOD']);
+
+//  });
+
+// Route::get('/read', function(){
+
+//     $results = DB::select('select * from posts where id = ?', [1]);
+
+//     return var_dump($results);
+//     // foreach($results as $posts){
+
+//     //     return $posts-> title;  
+//     // }
+
+// });
+
+// Route::get('/update', function(){
+
+// $updated = DB::update('update posts set title = "update title" where id = ?', [1]);
+
+// return $updated;
+// });
+
+// Route::get('/delete', function(){
+
+//     $deleted = DB::delete('delete from posts where id = ?', [1]);
+//     return $deleted;
+// });
+
+/////////////////////--------------ELOQUENT ORM --------------------------------//////////////////////
+
+//  Route::get('/read', function(){
+
+//        $posts = Post::all();
+
+//        foreach($posts as $post){
+
+//         return $post->title;
+
+//        }
+// });
+
+// Route::get('/find', function(){
+
+//     $post = Post::find(2);
+
+//     return $post->content;
+
+// });
+// Route::get('/findwhere', function(){
+
+
+// $posts = Post::where('id',3)->orderBy('id','desc')->take(1)->get();
+
+// return $posts;
+
+// // });
+// Route::get('/findmore', function(){
+
+// // $posts = Post::findOrFail(2);
+
+// // return $posts;
+
+// $posts = Post::where('users_count', '<', 50)->firstOrFail();
+
+// });
+
+//  Route::get('/besicinsert', function(){
+
+//      $post = new Post;
+
+//      $post->title = 'New Eloquent title insert';
+//      $post->content = 'Wow eloquent is really cool,look at this content';
+//      $post->save();
+    
+//  });
+
+// Route::get('/besicinsert2', function(){
+
+//        $post = Post::find(2);
+    
+//        $post->title = 'New Eloquent title insert no. 2';
+//          $post->content = 'Wow eloquent is really cool,look at this content no.2';
+//          $post->save();
+        
+//      });
+
+//  Route::get('/create', function (){
+
+//  Post::create(['title'=>'the create method', 'content'=>'Wow I\'am learning alot with Edwin diaz']);
+
+
+//  });
+
+// Route::get('/update', function (){
+
+// Post::where('id', 2)->where('is_admin',0)->update(['title'=>'NEW PHP TITLE', 'content'=>'I love my instructor Edwin']);
+
+// });
+// Route::get('/delete', function(){
+
+// $post = Post::find(2);
+
+//  $post->delete();
+
+// });
+// Route::get('/delete2', function(){
+
+//     Post::destroy([4,5]);
+
+//     // Post::where('is_admin',0)->delete();
+
+// });
+
+//  Route::get('/softdelete', function(){
+
+//   Post::find(6)->delete();
+
+//    });
+// Route::get('/readsoftdelete', function(){
+
+// $post = Post::find(6);
+// return $post;
+
+// $post = Post::withTrashed()->where('id',6)->get();
+// return $post;
+// $post = Post::onlyTrashed()->where('is_admin', 0)->get();
+//  return $post;
+
+// });
+// Route::get('/restore', function(){
+
+// Post::withTrashed()->where('is_admin',0)->restore();
+
+// });
+// Route::get('/forcedelete', function(){
+
+//     Post::withTrashed()->where('is_admin',0)->forceDelete();
+
+// });
